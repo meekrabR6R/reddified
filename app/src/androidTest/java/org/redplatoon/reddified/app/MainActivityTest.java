522cc45e8e21d50e76be7a56cd65f125716b8a27
Nick Miano <nmiano84@gmail.com>
2014-06-05 15:39:31 -0400

    more specs

--- app/src/androidTest/java/org/redplatoon/reddified/app/MainActivityTest.java
+++ app/src/androidTest/java/org/redplatoon/reddified/app/MainActivityTest.java
@@ -1,15 +1,20 @@
 package org.redplatoon.reddified.app;
 
 import android.app.Activity;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
 
 /**
  * Created by nmiano on 6/5/14 2:06 PM for Reddified
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
     private Activity mActivity;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ViewPager mViewPager;
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -19,14 +24,21 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     protected void setUp() throws Exception {
         super.setUp();
         mActivity = getActivity();
+        mDrawerLayout = (DrawerLayout) mActivity.findViewById(R.id.drawer);
+        mDrawerList = (ListView) mActivity.findViewById(R.id.left_drawer);
+        mViewPager = (ViewPager) mActivity.findViewById(R.id.pager);
     }
 
     public void testLayout() throws Exception {
-        DrawerLayout drawerLayout = (DrawerLayout) mActivity.findViewById(R.id.drawer);
-        assertNotNull("Drawer not allowed to be null", drawerLayout);
+        assertNotNull("Drawer not allowed to be null", mDrawerLayout);
+        assertNotNull("Drawer list not allowed to be null", mDrawerList);
+        assertNotNull("ViewPager not allowed to be null", mViewPager);
+        assertEquals(true, mActivity.getActionBar().isShowing());
+    }
 
-        ViewPager viewPager = (ViewPager) mActivity.findViewById(R.id.pager);
-        assertNotNull("ViewPager not allowed to be null", viewPager);
-
+    public void testDrawerToggle() throws Exception {
+        assertEquals(false, mDrawerLayout.isDrawerOpen(GravityCompat.START));
+        mDrawerLayout.performClick();
+        //assertEquals(true, mDrawerLayout.isDrawerOpen(GravityCompat.START));
     }
 }
