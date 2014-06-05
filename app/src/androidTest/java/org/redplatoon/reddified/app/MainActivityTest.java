5aa7159fa3eb64ee3c940a55465a09409bdebbb4
Nick Miano <nmiano84@gmail.com>
2014-06-05 14:21:34 -0400

    add test directory

---
+++ app/src/androidTest/java/org/redplatoon/reddified/app/MainActivityTest.java
@@ -0,0 +1,14 @@
+package org.redplatoon.reddified.app;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by nmiano on 6/5/14 2:06 PM for Reddified
+ */
+public class MainActivityTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 1;
+        assertEquals(expected, reality);
+    }
+}
