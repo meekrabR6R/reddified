d5431b05c391ff722f6e8820c7ccf596abfcf399
Nick Miano <nmiano84@gmail.com>
2014-06-10 22:11:35 -0400

    Comment class

---
+++ app/src/main/java/org/redplatoon/reddified/app/models/Comment.java
@@ -0,0 +1,32 @@
+package org.redplatoon.reddified.app.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nmiano on 6/10/14 9:31 PM for Reddified
+ */
+public class Comment {
+    String id, author, body, name, distinguished;
+    boolean saved, edited;
+    int score, ups, downs, gilded, created;
+
+    @SerializedName("parent_id")
+    String parentId;
+    @SerializedName("body_html")
+    String bodyHtml;
+    @SerializedName("link_id")
+    String linkId;
+    @SerializedName("author_flair_text")
+    String flairText;
+    @SerializedName("num_reports")
+    String numReports;
+    @SerializedName("score_hidden")
+    boolean scoreHidden;
+    @SerializedName("created_utc")
+    int createdUtc;
+
+    private ArrayList<Comment> children = new ArrayList<Comment>();
+
+}
